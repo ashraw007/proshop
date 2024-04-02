07570ac93d4f564140ff9f15af0187ef95506277
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2024-04-02 12:18:40 +0530

    constant.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://e-commerce-44q1.onrender.com';
-//export const BASE_URL = 'https://e-commerce-44q1.onrender.com'; // If using proxy
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:5000'
+//     : 'https://e-commerce-44q1.onrender.com';
+export const BASE_URL = 'https://e-commerce-44q1.onrender.com'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';

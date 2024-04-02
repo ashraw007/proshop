054c4f5209bec98c8955a0a7f5ff36f3d4d89751
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2024-04-02 12:23:33 +0530

    constant.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,7 +3,8 @@
 //     ? 'http://localhost:5000'
 //     : 'https://e-commerce-44q1.onrender.com';
 export const BASE_URL = 'https://e-commerce-44q1.onrender.com'; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = 'https://e-commerce-44q1.onrender.com/api/products';
+export const USERS_URL = 'https://e-commerce-44q1.onrender.com/api/users';
+export const ORDERS_URL = 'https://e-commerce-44q1.onrender.com/api/orders';
+export const PAYPAL_URL =
+  'https://e-commerce-44q1.onrender.com/api/config/paypal';
